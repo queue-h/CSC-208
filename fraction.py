3a0a7a49982a1414548bd3743497562b5fc04c1a
Quinn Helfand <qhelfand@gmail.com>
2025-09-30 10:48:23 -0400

    classwork 9/30

--- fraction.py
+++ fraction.py
@@ -1,34 +1,71 @@
+from typing import override
+
 class Fraction:
-    def __init__(self, numerator, denominator = 1):
+    def __init__(self, numerator = 0, denominator = 1):
+        sign = 1
         if denominator == 0:
-            raise ZeroDivisionError("Denominator cannot be 0")
-        if numerator == 0:
+            raise ZeroDivisionError("Denominator cannot be zero.")
+        if numerator == 0:  # zero fraction stored as 0/1
             self._numerator = 0
             self._denominator = 1
-        else:
-            if (numerator > 0 and denominator >= 0) or (numerator >= 0 and denominator < 0):
-                self.sign = -1
+        else: #nonzero
+            if (numerator < 0 and denominator > 0) or (numerator >= 0 and denominator < 0):
+                sign = -1
             else:
-                self.sign = 1
-
+                sign = 1
+        # Calculate the GCD of numerator and denominator using
+        # Euclid's algorithm
         a = abs(numerator)
         b = abs(denominator)
-
         while a % b != 0:
             tempA = a
             tempB = b
             a = tempB
             b = tempA % tempB
+            
+        self._numerator = abs(numerator) // b * sign
+        self._denominator = abs(denominator) // b
 
-        self._numerator = abs(numerator)
-        self._denominator = abs(denominator)
+    def __eq__(self, other):
+        return (self._numerator == other._numerator and
+                self._denominator == other._denominator)
+    def __float__(self):
+        return self._numerator/self._denominator
 
-    def __eq__(self, rhs_value):
-        return (self._numerator == rhs_value.get_numerator()) and (self._denominator == rhs_value.get_denominator())
-
-    def get_numerator(self):
-        return self._numerator
-    def get_denominator(self):
-        return self._denominator
-
-
+    def __lt__(self, rhsValue) :
+        return (self._numerator * rhsValue._denominator <              
+                self._denominator * rhsValue._numerator)
+    def __add__(self, rhsValue):
+        if type(rhsValue) == int:
+            rhsValue = Fraction(rhsValue)
+        num = (self._numerator * rhsValue._denominator +
+               self._denominator * rhsValue._numerator)
+        den = self._denominator * rhsValue._denominator
+        return Fraction(num, den)
+    def __str__(self):
+        return str(self._numerator)+"/"+str(self._denominator)
+    def __gt__(self, rhsValue) :
+        return (self._numerator * rhsValue._denominator >              
+                self._denominator * rhsValue._numerator)   
+    def __sub__(self, rhsValue):
+        if type(rhsValue) == int:
+            rhsValue = Fraction(rhsValue)
+        num = (self._numerator * rhsValue._denominator - 
+               self._denominator * rhsValue._numerator)
+        den = self._denominator * rhsValue._denominator
+        return Fraction(num, den)
+    def __ne__(self, rhsValue):
+        return not self == rhsValue
+    def __le__(self, rhsValue):
+        return not self > rhsValue
+    def __mul__(self, rhsValue):
+        if type(rhsValue) == Fraction:
+            num = (self._numerator * rhsValue._numerator)
+            den = self._denominator * rhsValue._denominator
+            return Fraction(num, den)
+        if type(rhsValue) ==  int:
+            num = (self._numerator * rhsValue)
+            den = self._denominator
+            return Fraction(num, den)
+        else:
+            raise TypeError("multiply only int and Fraction values")
---
+++ fraction_runner.py
@@ -0,0 +1,14 @@
+from fraction import Fraction
+
+frac1 = Fraction(1, 2)
+frac2 = Fraction(2, 3)
+frac3 = Fraction(3, 4)
+
+print(frac1)
+print(frac2 == frac3)
+print(frac1 > frac2)
+print(frac1 * frac2)
+print(frac1 != frac2)
+print(frac1 + 3)
+print(float(frac3))
+print(frac3 - frac1)
