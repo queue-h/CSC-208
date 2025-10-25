ca95e974e262bf15884c25bed310dad9d797492c
Quinn Helfand <qhelfand@gmail.com>
2025-10-25 15:57:23 -0400

    the ball bounces i guess

--- pygame/bouncing_ball.py
+++ pygame/bouncing_ball.py
@@ -2,6 +2,7 @@ import pygame
 from pygame.locals import *
 import pygame_widgets
 from pygame_widgets.slider import Slider
+from pygame_widgets.textbox import TextBox
 import sys
 # define constants
 BLACK = (0, 0, 0)
@@ -9,6 +10,18 @@ RED = (255, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
+
+# slider sizing
+slider_horizontal_padding = 25
+slider_width = (WINDOW_WIDTH - slider_horizontal_padding * 4) // 2
+slider_height = WINDOW_HEIGHT // 24
+SLIDER_MIN = 1
+SLIDER_MAX = 30
+SLIDER_STEP = 1
+SLIDER_PADDING = slider_height + 30
+slider_y = 10
+x_slider_x = slider_horizontal_padding
+y_slider_x = slider_width + slider_horizontal_padding * 3
 
 # initialize the work
 pygame.init()
@@ -18,11 +31,34 @@ X_SPEED = 5
 Y_SPEED = 5
 RADIUS = 10
 
-x, y = (WINDOW_WIDTH / 2, WINDOW_HEIGHT / 2)
+x_slider = Slider(window, x_slider_x, slider_y, slider_width, slider_height, min=SLIDER_MIN, max=SLIDER_MAX, step=SLIDER_STEP)
+y_slider = Slider(window, y_slider_x, slider_y, slider_width, slider_height, min=SLIDER_MIN, max=SLIDER_MAX, step=SLIDER_STEP)
+x_label = TextBox(window, x_slider_x, slider_y, 60, 25, fontSize=15)
+x_label.setText("X Speed")
+y_label = TextBox(window, y_slider_x, slider_y, 60, 25, fontSize=15)
+y_label.setText("Y Speed")
+
+negative_y = False
+negative_x = False
+
+x, y = (WINDOW_WIDTH / 2, (WINDOW_HEIGHT - SLIDER_PADDING) / 2)
 
 while True:
-    #window.fill(BLACK)
-    for event in pygame.event.get():
+
+    # adjust speed according to slider
+    if negative_x:
+        X_SPEED = -x_slider.value
+    else:
+        X_SPEED = x_slider.value
+    if negative_y:
+        Y_SPEED = -y_slider.value
+    else:
+        Y_SPEED = y_slider.value
+
+
+    window.fill(BLACK)
+    events = pygame.event.get()
+    for event in events:
         # if close clicked, quit and end program
         if event.type == pygame.QUIT:
                 pygame.quit()
@@ -40,12 +76,22 @@ while True:
     if x - RADIUS <= 0:
         X_SPEED = -X_SPEED
     # hits floor
-    if y + RADIUS >= WINDOW_HEIGHT:
+    if y + RADIUS >= WINDOW_HEIGHT :
         Y_SPEED = -Y_SPEED
     # hits ceiling
-    if y - RADIUS <= 0:
+    if y - RADIUS <= SLIDER_PADDING:
         Y_SPEED = -Y_SPEED
 
-    pygame.display.update()
+    # determine whether x and y is negative and boolean
+    if Y_SPEED < 0:
+        negative_y = True
+    else:
+        negative_y = False
+    if X_SPEED < 0:
+        negative_x = True
+    else:
+        negative_x = False
 
+    pygame_widgets.update(events)
+    pygame.display.update()
     clock.tick(FRAMES_PER_SECOND)
--- pygame/pygame_template.py
+++ pygame/pygame_template.py
@@ -1,29 +1,51 @@
+# pygame demo 0 - window only
+
+# 1 - Import packages
 import pygame
 from pygame.locals import *
 import sys
-# define constants
+
+# 2 - Define constants
 BLACK = (0, 0, 0)
 WINDOW_WIDTH = 640
 WINDOW_HEIGHT = 480
 FRAMES_PER_SECOND = 30
 
-# initialize the work
+# 3 - Initialize the world
 pygame.init()
 window = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+
+# Fullscreen mode, but DO NOT switch to this until you've figured
+# out how to use a key event to exit the program. There is no close window
+# button when pygame runs in fullscreen mode - you will be stuck.
+
+# window = pygame.display.set_mode((0, 0), pygame.FULLSCREEN)
+
 clock = pygame.time.Clock()
 
-while True:
-    for event in pygame.event.get():
-        # if close clicked, quit and end program
-        if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+# 4 - Load assets: image(s), sounds(s),  etc.
 
+# 5 - Initialize variables
+
+# 6 - Loop forever
+while True:
+
+    # 7 - Check for and handle events
+    for event in pygame.event.get():
+        # Clicked the close button? Quit pygame and end program
+        if event.type == pygame.QUIT:
+            pygame.quit()
+            sys.exit()
+
+    # 8 - Do any "per frame" actions
+
+    # 9 - Clear the window
     window.fill(BLACK)
 
+    # 10 - Draw all window elements
+
+    # 11 - Update the window
     pygame.display.update()
 
-    clock.tick(FRAMES_PER_SECOND)
-
-
-
+    # 12 - Slow things down a bit
+    clock.tick(FRAMES_PER_SECOND)  # make pygame wait
