e8e660117330cf65d52873b13f7e1aa12e972512
Quinn Helfand <qhelfand@gmail.com>
2025-12-01 13:37:46 -0500

    sorting and searching

--- sort_and_search/find_min.py
+++ sort_and_search/find_min.py
@@ -11,16 +11,19 @@ def linear_min(nums):
     return f"Minimum = {min}\nNumber of Seconds = {(end-start).total_seconds()}"
 
 def exponential_min(nums):
+    num_iter = 0
     start = datetime.now()
+    min = nums[0]
     for index1 in range(len(nums)):
         for index2 in range(index1):
-            if nums[index1] < nums[index2]:
+            num_iter += 1
+            if nums[index1] < min:
                 min = nums[index1]
-            else:
+            if nums[index2] < min:
                 min = nums[index2]
     end = datetime.now()
     return f"Minimum = {min}\nNumber of Seconds = {(end-start).total_seconds()}"
 
-nums = np.random.uniform(low=-1000, high=1000, size=10000)
+nums = np.random.uniform(low=-1000, high=1000, size=1000)
 print(linear_min(nums))
 print(exponential_min(nums))
